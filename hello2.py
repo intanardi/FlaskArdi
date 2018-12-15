de78aabd32667d51be5ed5d43f1b453d915f60fb
ardi <intan.ardipramanova@gmail.com>
2018-12-15 09:04:31 +0700

    first commit

---
+++ hello2.py
@@ -0,0 +1,75 @@
+from flask import Flask, render_template, session, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
+import os
+from flask_script import Shell
+from flask_migrate import Migrate, MigrateCommand
+# ...
+
+# from flask_script import Manager
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'megadeth15'
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+# file_path = os.path.abspath(os.getcwd())+"\database.db"
+
+
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///dbdir/test.db'
+
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///'+file_path
+app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS']= False
+
+# app.config.from_object(Config)
+db = SQLAlchemy(app)
+migrate = Migrate(app, db) # this
+
+class Role(db.Model):
+    __tablename__ = 'roles'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64), unique=True)
+    # relationships
+    users = db.relationship('User', backref='role')
+    def __repr__(self):
+        return '<Role %r>' % self.name
+
+
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), unique=True, index=True)
+    # relationships
+    role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
+
+    def __repr__(self):
+        return '<User %r>' % self.username
+
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    form = NameForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.name.data).first()
+        if user is None:
+            user = User(username = form.name.data)
+            db.session.add(user)
+            session['known'] = False
+        else:
+            session['known'] = True
+        session['name'] = form.name.data
+        form.name.data = ''
+        return redirect(url_for('index'))
+    return render_template('index.html',
+        form = form, name = session.get('name'),
+        known = session.get('known', False))
+
+def make_shell_context():
+    return dict(app=app, db=db, User=User, Role=Role)
+    manager.add_command("shell", Shell(make_context=make_shell_context))
+    manager.add_command('db', MigrateCommand)
+
+#
+if __name__ == '__main__':
+    app.run(debug=True)
