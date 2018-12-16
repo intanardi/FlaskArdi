78e422a7b336ac801ac073c15b136a46ce4be963
Intan Ardipramanova <intan.ardipramanova@gmail.com>
2018-12-16 20:28:34 +0700

    first flask commited

---
+++ config.py
@@ -0,0 +1,37 @@
+import os
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+
+class Config:
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'hard to guess string'
+    SSL_DISABLE = False
+    SQLALCHEMY_COMMIT_ON_TEARDOWN = True
+    SQLALCHEMY_RECORD_QUERIES = True
+    MAIL_SERVER = 'smtp.googlemail.com'
+    FLASKY_MAIL_SUBJECT_PREFIX = '[Flasky]'
+    FLASKY_MAIL_SENDER = 'Flasky Admin <flasky@example.com>'
+    FLASKY_DB_QUERY_TIMEOUT=0.5
+
+    @staticmethod
+    def init_app(app):
+        pass
+
+
+class DevelopmentConfig(Config):
+    DEBUG = True
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DEV_DATABASE_URL') or \
+        'sqlite:///' + os.path.join(basedir, 'data-dev.sqlite')
+
+
+class TestingConfig(Config):
+    TESTING = True
+    SQLALCHEMY_DATABASE_URI = os.environ.get('TEST_DATABASE_URL') or \
+        'sqlite:///' + os.path.join(basedir, 'data-test.sqlite')
+    WTF_CSRF_ENABLED = False
+
+
+config = {
+    'development': DevelopmentConfig,
+    'testing': TestingConfig,
+    'default': DevelopmentConfig
+}
--- hello2.py
+++ hello2.py
@@ -1,50 +1,82 @@
 from flask import Flask, render_template, session, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 import os
-from flask_script import Shell
+from flask_script import Shell, Manager
 from flask_migrate import Migrate, MigrateCommand
-# ...
+from flask_mail import Mail
 
-# from flask_script import Manager
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'megadeth15'
 
-basedir = os.path.abspath(os.path.dirname(__file__))
+# basedir = os.path.abspath(os.path.dirname(__file__))
+
 # file_path = os.path.abspath(os.getcwd())+"\database.db"
-
-
-
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'data.sqlite')
 # app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///dbdir/test.db'
 
 # app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///'+file_path
-app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS']= False
+
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+# app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
+# app.config['SQLALCHEMY_TRACK_MODIFICATIONS']= False
+
+app.config['FLASKY_ADMIN'] = os.environ.get('FLASKY_ADMIN')
+app.config['MAIL_SERVER'] = 'intan.ardipramanova@gmail.com'
+app.config['MAIL_PORT'] = 25
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USERNAME'] = os.environ.get('MAIL_USERNAME')
+app.config['MAIL_PASSWORD'] = os.environ.get('MAIL_PASSWORD')
+app.config['FLASKY_MAIL_SUBJECT_PREFIX'] = '[Flasky]'
+app.config['FLASKY_MAIL_SENDER'] = 'Flasky Admin <flasky@example.com>'
+
+def send_email(to, subject, template, **kwargs):
+    msg = Message(app.config['FLASKY_MAIL_SUBJECT_PREFIX'] + subject,
+        sender=app.config['FLASKY_MAIL_SENDER'], recipients=[to])
+    msg.body = render_template(template + '.txt', **kwargs)
+    msg.html = render_template(template + '.html', **kwargs)
+    mail.send(msg)
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    form = NameForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.name.data).first()
+        if user is None:
+            user = User(username=form.name.data)
+            db.session.add(user)
+            session['known'] = False
+            if app.config['FLASKY_ADMIN']:
+            send_email(app.config['FLASKY_ADMIN'], 'New User','mail/new_user', user=user)
+        else:
+            session['known'] = True
+        session['name'] = form.name.data
+        form.name.data = ''
+        return redirect(url_for('index'))
+    return render_template('index.html', form=form, name=session.get('name'),known=session.get('known', False))
 
 # app.config.from_object(Config)
-db = SQLAlchemy(app)
-migrate = Migrate(app, db) # this
+# db = SQLAlchemy(app)
 
-class Role(db.Model):
-    __tablename__ = 'roles'
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(64), unique=True)
-    # relationships
-    users = db.relationship('User', backref='role')
-    def __repr__(self):
-        return '<Role %r>' % self.name
-
-
-class User(db.Model):
-    __tablename__ = 'users'
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(64), unique=True, index=True)
-    # relationships
-    role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
-
-    def __repr__(self):
-        return '<User %r>' % self.username
+# class Role(db.Model):
+#     __tablename__ = 'roles'
+#     id = db.Column(db.Integer, primary_key=True)
+#     name = db.Column(db.String(64), unique=True)
+#     # relationships
+#     users = db.relationship('User', backref='role')
+#     def __repr__(self):
+#         return '<Role %r>' % self.name
+#
+#
+# class User(db.Model):
+#     __tablename__ = 'users'
+#     id = db.Column(db.Integer, primary_key=True)
+#     username = db.Column(db.String(64), unique=True, index=True)
+#     # relationships
+#     role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
+#
+#     def __repr__(self):
+#         return '<User %r>' % self.username
 
 
 @app.route('/', methods=['GET', 'POST'])
@@ -65,11 +97,17 @@ def index():
         form = form, name = session.get('name'),
         known = session.get('known', False))
 
+manager = Manager(app)
+# migrate = Migrate(app, db)
+mail = Mail(app)
+
 def make_shell_context():
     return dict(app=app, db=db, User=User, Role=Role)
-    manager.add_command("shell", Shell(make_context=make_shell_context))
-    manager.add_command('db', MigrateCommand)
+
+manager.add_command("shell", Shell(make_context=make_shell_context))
+# manager.add_command('db', MigrateCommand)
 
 #
 if __name__ == '__main__':
+    # manager.run()
     app.run(debug=True)
